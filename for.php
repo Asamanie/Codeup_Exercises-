4782035f5af6a7854036bd88abab1f1d79e3f91b
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-13 12:29:50 -0500

    finished my for statement

--- for.php
+++ for.php
@@ -1,19 +1,39 @@
 <?php
 
-if ($argc < 2) {
-	$numbers = $argv[0];
-	echo "Usage: Please pick a starting number and ending number\n";
-	exit(1);
+// greet the user  
+echo "Hello!\n";
+
+	// ask user to input number 
+	fwrite (STDOUT, 'please pick a number ');
+
+	// Get the input from user
+	$number1 = trim(fgets(STDIN));
+	if (!is_numeric($number1)){
+	echo "Must be a number!!\n";
+	exit;
+	}
+
+	// ask user to input second number 
+	fwrite(STDOUT, 'please pick another number ');
+
+	// Get the input from user
+	$number2 = fgets(STDIN);
+	if (!is_numeric($number2)){
+	echo "Must be a number!!\n";
+	exit;
+	}
+	
+	// ask how the user wants the numbers to increment 
+	fwrite(STDOUT, 'what would you like your number to increment by ? ');
+	$i = fgets(STDIN);
+
+		// take the numbers given and add by increment until $number 2 hit
+		for ($number1 = $number1; $number1 <= $number2; $number1 = $number1 + $i){
+    	echo "{$number1}\n";
 }
 
-$choice1 = $argv[1];
-$choice2 = $argv[2];
 
 
-echo $choice1;
-
-for ($choice1 = $choice1++){
-	echo "{$choice1}\n";
 
 
-}
+	
