2666ce4b31aa7bc88ecf44bb7f0e38e51660fba5
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-13 16:16:11 -0500

    fixed alignment and wording

--- foreach.php
+++ foreach.php
@@ -3,20 +3,30 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-   	echo "this is {$thing}\n";
 
-	if (is_int($thing)) {
-		echo "This is a Integer";
-	} elseif (is_float($thing)) {
-		echo "This is a Float";
-	} elseif (is_bool($thing)) {
-		echo "This is a Boolean";
-	} elseif (is_array($thing)) {
-		echo "This is a Array";
-	} elseif (is_null($thing)) {
-		echo "This is NULL";
-	} elseif (is_string($thing)) {
-			 "This is a String";
+	if (is_int($thing)) 
+	{
+		echo "{$thing} is a Integer\n";
+	} 
+	elseif (is_float($thing)) 
+	{
+		echo "{$thing} is a Float\n";
+	} 
+	elseif (is_bool($thing))
+	{
+		echo "{$thing} is a Boolean\n";
+	} 
+	elseif (is_array($thing))
+	{
+		echo "{$thing} is a Array\n";
+	}
+	elseif (is_null($thing)) 
+	{
+		echo "{$thing} is NULL\n";
+	} 
+	elseif (is_string($thing))
+	{
+		echo "{$thing} is a String\n";
 	}
 
 }
