19087573149efaa58c7557e68bb3e07575ea890a
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-14 11:26:33 -0500

    add break to code

--- break_continue.php
+++ break_continue.php
@@ -1,8 +1,17 @@
 <?php
 
-for ($i = 0; $i <= 100; $i++){
+// // 
+
+// for ($i = 2; $i <= 100; $i += 2){
+// 		echo $i . "\n";
+// 		if ($i % 2 == 0){
+// 			continue;
+// 		}echo "^ this number is Even\n";
+// }
+
+for ($i = 1; $i <= 100; $i++) {
 		echo $i . "\n";
-		if ($i % 2 == 0){
-			echo "^ this number is Even\n";
+		if ($i == 10){
+			break;
 		}
 }
