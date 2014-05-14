88a248c48a9ecda889d381ecc1a2a74d92b48fb8
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-14 08:41:18 -0500

    add files to git

--- fruit.php
+++ fruit.php
@@ -1,7 +1,26 @@
 <?php
+// for loop
 
-$fruit = [apple, orange, grape, lime, lemon];
+// $fruits = array('apple', 'orange', 'grape', 'lime', 'lemon');
+// 	for ($i = 0; $i < count($fruits); $i++){
+// 		echo $fruits{$i} . PHP_EOL;
+// }
 
-foreach ($fruit as $key => $value) {
-    echo ("\$value has a key of {$key} and a value of {$value}\n");
-}
+// foreach loop
+
+// $fruits = array('apple', 'orange', 'grape', 'lime', 'lemon');
+// 	foreach ($fruits as $fruit)
+// 	{
+// 		echo "$name\n";	
+// 	}
+
+$fruits = array(
+    		array('name' => 'apple', 'color' => 'red'),
+    		array('name' => 'orange', 'color' => 'organge'),
+    		array('name' => 'grape', 'color' => 'purple'),
+    		array('name' => 'lime', 'color' => 'green'),
+    		array('name' => 'lemon', 'color' => 'yellow')
+    	);
+	foreach ($fruits as $fruit) {
+		echo $fruit['name'] . " is "  . $fruit['color'] . PHP_EOL;
+	}
