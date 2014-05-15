4951d3b5f669190ed80ff5d33f52dd292aad4d4c
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-15 14:09:59 -0500

    adding 3 part of exercise

--- arithmetic.php
+++ arithmetic.php
@@ -1,8 +1,9 @@
 <?php
 
 
-// adding
-// function add($a, $b) {
+// addition
+
+// function adder($a, $b) {
 //     if (is_numeric($a) && is_numeric($b)){
 //    		echo $a + $b; 
 //    	} else {
@@ -11,7 +12,7 @@
 //      echo PHP_EOL;
 // }
 
-// add(a,4);
+// adder(3,4);
 
 
 
@@ -29,23 +30,36 @@
 
 
 
-function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)){
-    echo $a * $b;
-		} else }
-		 echo 'ERROR: $a or $b must be a number';
-		}
-		echo PHP_EOL; 
-}
-multiply(a,4);
+// function multiply($a, $b) {
+//     if (is_numeric($a) && is_numeric($b)){
+//     echo $a * $b;
+// 		} else }
+// 		 echo 'ERROR: $a or $b must be a number';
+// 		}
+// 		echo PHP_EOL; 
+// }
+// multiply(a,4);
 
 
-function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b)){
-    echo $a / $b
-	} else }
-	  echo 'ERROR: $a or $b must be a number';
-	}
-	echo PHP_EOL; 
+// function divide($a, $b) {
+//     if (is_numeric($a) && is_numeric($b)){
+//     echo $a / $b
+// 	} else }
+// 	  echo 'ERROR: $a or $b must be a number';
+// 	}
+// 	echo PHP_EOL; 
+// }
+// divide(20,2);
+
+
+
+if ($strict === true) {
+ 		echo $a === $b ? 'True' : 'False';
+ 	} else {
+ 		echo $a == $b ? 'True' : 'False';
+ 	}
+ 	echo PHP_EOL;
 }
-divide(20,2);
+ compare(42, 42, true);
+
+
