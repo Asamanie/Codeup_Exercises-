20171e19288bb624278450dd673b1afc43fccc99
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-15 13:22:37 -0500

    added arguements to the code

--- arithmetic.php
+++ arithmetic.php
@@ -2,23 +2,36 @@
 
 
 // adding
-function add($a, $b) {
-    echo $a + $b . PHP_EOL;
-}
-add(2,4);
+// function subtract($a, $b) {
+//     if (is_numeric($a) && is_numeric($b)){
+//    		echo $a + $b; 
+//    	} else {
+//    	   	echo 'ERROR: $a or $b must be a number';	
+//      }
+//      echo PHP_EOL;
+// }
 
- function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
-}
- subtract(10,2);
+// subtract(a,4);
+
+
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    echo $a - $b;
+		} else }
+		 echo 'ERROR: $a or $b must be a number';
+		}
+		echo PHP_EOL; 
 }
- multiply(2,4);
+multiply(a,4);
 
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL; 
+    if (is_numeric($a) && is_numeric($b)){
+    echo $a / $b
+	} else }
+	  echo 'ERROR: $a or $b must be a number';
+	}
+	echo PHP_EOL; 
 }
- divide(20,2);
+divide(20,2);
