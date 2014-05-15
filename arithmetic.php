3a8af815a709a583619fedf9bb687eb7b819172a
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-15 10:03:31 -0500

    first commit to functions folder

---
+++ arithmetic.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// adding
+function addrer($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+addrer(2,4);
+
+ function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+ subtract(10,2);
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+ multiply(2,4);
+
+
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL; 
+}
+ divide(20,2);
