1be8d535f0bc840a58fa6f6ba045a218ddd984ee
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-31 15:45:36 -0500

    I have restarted the codeup exercises for practice and review code from class starts on (line 27)

--- if_and_or.php
+++ if_and_or.php
@@ -4,24 +4,50 @@ $x = 0;
 $y = 5;
 $z = 10;
 
-// if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+//#1 - if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+if ($x < $y || $y <$z);
+echo "{$x} < {$y} < {$z}\n";
 
-if ($x < $y && $y < $z){
-	echo "$x is greater than 0 OR less than 10" . "\n";
-}
-// if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
-
+//#2 - if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
 // repeat the if statement for $y and $z.
+if ($x > $x || $x < $z);
+echo "$x is greater than 0 or less than 10\n";
+
+//#3 - If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
+if($y > $x || $x < $z);
+echo "$x is greater than $x and $x is less than $z\n";
+
+if($z > $y && $x < $y);
+echo "$z is greater than $y and $y is greather than $x\n";
 
 
-if ($x > 0 || $x < 10){
-	echo "$x is greater than 0 AND less than 10\n";
-}
-// If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
 
-// repeat the if statement for $y and $z.
 
-if ($x > 0 && $x < 10){
-	echo "$x is greater than 0 AND less than 10\n";
-}
-?>
+
+// The code below are exercises from Codeup class.  I have gone back and started the exercises again
+// for practice and refresher.
+// 
+// $x = 0;
+// $y = 5;
+// $z = 10;
+
+// // if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+
+// if ($x < $y && $y < $z){
+// 	echo "$x is greater than 0 OR less than 10" . "\n";
+// }
+// // if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+
+// // repeat the if statement for $y and $z.
+
+
+// if ($x > 0 || $x < 10){
+// 	echo "$x is greater than 0 AND less than 10\n";
+// }
+// // If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
+
+// // repeat the if statement for $y and $z.
+
+// if ($x > 0 && $x < 10){
+// 	echo "$x is greater than 0 AND less than 10\n";
+// }
