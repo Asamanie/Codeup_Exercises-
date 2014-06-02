94fe86c134de51557d402d94cec3216e3c6b9a94
Andrew Samaniego <andrewsamaniego@a-s.gateway.pace.com>
2014-06-01 23:46:05 -0500

    reworked the the exercise to echo results through a foreach loop which is mush shorter and less code written

--- foreach.php
+++ foreach.php
@@ -2,33 +2,8 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+//loop through array called $things and echo's back the result. 
 foreach ($things as $thing) {
-
-	if (is_int($thing)) 
-	{
-		echo "{$thing} is a Integer\n";
-	} 
-	elseif (is_float($thing)) 
-	{
-		echo "{$thing} is a Float\n";
-	} 
-	elseif (is_bool($thing))
-	{
-		echo "{$thing} is a Boolean\n";
-	} 
-	elseif (is_array($thing))
-	{
-		echo "{$thing} is a Array\n";
-	}
-	elseif (is_null($thing)) 
-	{
-		echo "{$thing} is NULL\n";
-	} 
-	elseif (is_string($thing))
-	{
-		echo "{$thing} is a String\n";
-	}
-
-}
-
+	echo "{$thing}\n";
+}	
 
