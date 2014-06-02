1107eac4dd306c989e4d99f2ba29258a138b61ba
Andrew Samaniego <andrewsamaniego@a-s.gateway.pace.com>
2014-06-01 21:55:53 -0500

    Ask user to pick 1 low number and 1 high number loop through to count from low number to high number by 1

--- for.php
+++ for.php
@@ -1,36 +1,66 @@
 <?php
 
-// greet the user  
-echo "Hello.........\n";
+// ask the user for a starting number 
+fwrite(STDOUT, "Hello....Please pick a low number.\n");
+// get the first number from the user
+$first_number = trim(fgets(STDIN));
+//ask user to pick a second number 
+fwrite(STDOUT, "Please pick high number.\n");
+// get the second number from the user
+$second_number = trim(fgets(STDIN));
 
-	// ask user to input number, if not a number close program 
-	fwrite (STDOUT, 'please pick a number ');
-
-	// Get the input from user
-	$number1 = trim(fgets(STDIN));
-	if (!is_numeric($number1)){
-	echo "Must be a number!!\n";
-	exit;
-	}
-
-	// ask user to input second number, if not a number close program
-	fwrite(STDOUT, 'please pick another number ');
-
-	// Get the input from user
-	$number2 = fgets(STDIN);
-	if (!is_numeric($number2)){
-	echo "Must be a number!!\n";
-	exit;
-	}
-	
-	// ask how the user wants the numbers to increment 
-	fwrite(STDOUT, 'what would you like your number to increment by ? ');
-	$i = fgets(STDIN);
-
-		// take the numbers given and add by increment until $number 2 hit
-		for ($number1 = $number1; $number1 <= $number2; $number1 = $number1 + $i){
-    	echo "{$number1}\n";
+// loop through w/ a FOR loop to count from low to high
+for ($first_number = $first_number; $first_number <= $second_number; $first_number++) { 
+		echo "$first_number\n";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Below is code that was done during codeup class
+// I have reviewed the exercises and will push to git hub again
+
+// greet the user  
+// echo "Hello.........\n";
+
+// 	// ask user to input number, if not a number close program 
+// 	fwrite (STDOUT, 'please pick a number ');
+
+// 	// Get the input from user
+// 	$number1 = trim(fgets(STDIN));
+// 	if (!is_numeric($number1)){
+// 	echo "Must be a number!!\n";
+// 	exit;
+// 	}
+
+// 	// ask user to input second number, if not a number close program
+// 	fwrite(STDOUT, 'please pick another number ');
+
+// 	// Get the input from user
+// 	$number2 = fgets(STDIN);
+// 	if (!is_numeric($number2)){
+// 	echo "Must be a number!!\n";
+// 	exit;
+// 	}
+	
+// 	// ask how the user wants the numbers to increment 
+// 	fwrite(STDOUT, 'what would you like your number to increment by ? ');
+// 	$i = fgets(STDIN);
+
+// 		// take the numbers given and add by increment until $number 2 hit
+// 		for ($number1 = $number1; $number1 <= $number2; $number1 = $number1 + $i){
+//     	echo "{$number1}\n";
+// }
 
 
 
